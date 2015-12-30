852249595f46f7156da75048ff759d959edcd5cb
Laura Haverkamp <laura@haverkamp.us>
2015-12-30 14:58:42 -0600

    fixed local copy to handle SSL certificates

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 /* required libraries */
 var path = require('path');
 var express = require('express');
+var ws = require('ws');
 var minimist = require('minimist');
 var url = require('url');
-var ws = require('ws');
 var kurento = require('kurento-client');
+var fs = require('fs');
+var https = require('https');
 
 /* command line arguments */
 var argv = minimist(process.argv.slice(2), {
@@ -14,14 +16,20 @@ var argv = minimist(process.argv.slice(2), {
 	}
 });
 
+var options = {
+	key: fs.readFileSync('keys/server.key'),
+	cert: fs.readFileSync('keys/server.crt')
+};
+
 /* application */
 var app = express();
 app.use(express.static(path.join(__dirname, 'static')));
 
-/* Global Variables */
+/* global variables */
 var kurentoClient = null;
 var userRegistry = new UserRegistry();
 var pipelines = {};
+var candidatesQueue = {};
 var idCounter = 0;
 
 function nextUniqueId() {
@@ -30,8 +38,9 @@ function nextUniqueId() {
 	return idCounter.toString();
 }
 
-/** 
+/**
  * This class represents the caller and callee sessions
+ * 
  * @param id
  * @param name
  * @param ws
@@ -82,81 +91,106 @@ UserRegistry.prototype.getByName = function(name) {
 };
 
 function CallMediaPipeline() {
-	this._pipeline = null;
-	this._callerWebRtcEndpoint = null;
-	this._calleeWebRtcEndpoint = null;
+	this.pipeline = null;
+	this.webRtcEndpoint = {};
 };
 
-CallMediaPipeline.prototype.createPipeline = function(callback) {
-	var self = this;
-	getKurentoClient(function(error, kurentoCLient) {
-		if(error) {
-			return callback(error);
-		}
-		
-		kurentoClient.create('MediaPipeline', function(error, pipeline) {
-			if(error) {
-				return callback(error);
-			}
-			
-			pipeline.create('WebRtcEndpoint', function(error, callerWebRtcEndpoint) {
-				if(error) {
-					pipeline.release();
-					
-					return callback(error);
-				}
-				
-				pipeline.create('WebRtcEndpoint', function(error, calleeWebRtcEndpoint) {
-					if(error) {
-						pipeline.release();
-						
-						return callback(error);
-					}
-					
-					callerWebRtcEndpoint.connect(calleeWebRtcEndpoint, function(error) {
-						if(error) {
-							pipeline.release();
-							
-							return callback(error);
-						}
+CallMediaPipeline.prototype.createPipeline = function(callerId, calleeId, ws, callback) {
+    var self = this;
+    getKurentoClient(function(error, kurentoClient) {
+        if(error) {
+        	return callback(error);
+        }
 
-						calleeWebRtcEndpoint.connect(callerWebRtcEndpoint, function(error) {
-							if(error) {
-								pipeline.release();
-								
-								return callback(error);
-							}
-						});
-						
-						self._pipeline = pipeline;
-						self._callerWebRtcEndpoint = callerWebRtcEndpoint;
-						self._calleeWebRtcEndpoint = calleeWebRtcEndpoint;
-					
-						callback(null);
-					});
-				});
-			});
-		});
-	});
+        kurentoClient.create('MediaPipeline', function(error, pipeline) {
+            if(error) {
+                return callback(error);
+            }
+
+            pipeline.create('WebRtcEndpoint', function(error, callerWebRtcEndpoint) {
+                if(error) {
+                    pipeline.release();
+                    return callback(error);
+                }
+
+                if(candidatesQueue[callerId]) {
+                    while(candidatesQueue[callerId].length) {
+                        var candidate = candidatesQueue[callerId].shift();
+                        callerWebRtcEndpoint.addIceCandidate(candidate);
+                    }
+                }
+
+                callerWebRtcEndpoint.on('OnIceCandidate', function(event) {
+                    var candidate = kurento.register.complexTypes.IceCandidate(event.candidate);
+                    userRegistry.getById(callerId).ws.send(JSON.stringify({
+                        id : 'iceCandidate',
+                        candidate : candidate
+                    }));
+                });
+
+                pipeline.create('WebRtcEndpoint', function(error, calleeWebRtcEndpoint) {
+                    if(error) {
+                        pipeline.release();
+                        
+                        return callback(error);
+                    }
+
+                    if(candidatesQueue[calleeId]) {
+                        while(candidatesQueue[calleeId].length) {
+                            var candidate = candidatesQueue[calleeId].shift();
+                            calleeWebRtcEndpoint.addIceCandidate(candidate);
+                        }
+                    }
+
+                    calleeWebRtcEndpoint.on('OnIceCandidate', function(event) {
+                        var candidate = kurento.register.complexTypes.IceCandidate(event.candidate);
+                        userRegistry.getById(calleeId).ws.send(JSON.stringify({
+                            id : 'iceCandidate',
+                            candidate : candidate
+                        }));
+                    });
+
+                    callerWebRtcEndpoint.connect(calleeWebRtcEndpoint, function(error) {
+                        if (error) {
+                            pipeline.release();
+                            
+                            return callback(error);
+                        }
+
+                        calleeWebRtcEndpoint.connect(callerWebRtcEndpoint, function(error) {
+                            if (error) {
+                                pipeline.release();
+                                
+                                return callback(error);
+                            }
+                        });
+
+                        self.pipeline = pipeline;
+                        self.webRtcEndpoint[callerId] = callerWebRtcEndpoint;
+                        self.webRtcEndpoint[calleeId] = calleeWebRtcEndpoint;
+                        callback(null);
+                    });
+                });
+            });
+        });
+    });
 };
 
-CallMediaPipeline.prototype.generateSdpAnswerForCaller = function(sdpOffer, callback) {
-	this._callerWebRtcEndpoint.processOffer(sdpOffer, callback);
-};
-
-CallMediaPipeline.prototype.generateSdpAnswerForCallee = function(sdpOffer, callback) {
-	this._calleeWebRtcEndpoint.processOffer(sdpOffer, callback);
+CallMediaPipeline.prototype.generateSdpAnswer = function(id, sdpOffer, callback) {
+    this.webRtcEndpoint[id].processOffer(sdpOffer, callback);
+    this.webRtcEndpoint[id].gatherCandidates(function(error) {
+        if (error) {
+            return callback(error);
+        }
+    });
 };
 
 CallMediaPipeline.prototype.release = function() {
-	if(this._pipeline) {
-		this._pipeline.release();
-	};
-	
-	this._pipeline = null;
+    if (this.pipeline) this.pipeline.release();
+    this.pipeline = null;
 };
 
-
+/* server startup */
 var asUrl = url.parse(argv.asUri);
 var port = asUrl.port;
 var server = app.listen(port, function() {
@@ -171,7 +205,6 @@ var wss = new ws.Server({
 
 wss.on('connection', function(ws) {
 	var sessionId = nextUniqueId();
-
 	console.log('Connection received with sessionId ' + sessionId);
 
 	ws.on('error', function(error) {
@@ -201,9 +234,13 @@ wss.on('connection', function(ws) {
 		case 'incomingCallResponse':
 			incomingCallResponse(sessionId, message.from, message.callResponse, message.sdpOffer);
 			break;
-		
+
 		case 'stop':
 			stop(sessionId);
+			break;
+
+		case 'onIceCandidate':
+			onIceCandidate(sessionId, message.candidate);
 			break;
 
 		default:
@@ -232,7 +269,7 @@ function register(id, name, ws, callback) {
 	}
 	
 	if(userRegistry.getByName(name)) {
-		return onError("username already registered");
+		return onError("user " + name + " is already registered");
 	}
 	
 	userRegistry.register(new UserSession(id, name, ws));
@@ -248,6 +285,8 @@ function register(id, name, ws, callback) {
 };
 
 function call(callerId, to, from, sdpOffer) {
+    clearCandidatesQueue(callerId);
+
 	var caller = userRegistry.getById(callerId);
 	var rejectCause = 'user ' + to + ' is not registered';
 	
@@ -279,6 +318,8 @@ function call(callerId, to, from, sdpOffer) {
 };
 
 function incomingCallResponse(calleeId, from, callResponse, calleeSdp) {
+    clearCandidatesQueue(calleeId);
+
 	function onError(callerReason, calleeReason) {
 		if(pipeline) {
 			pipeline.release();
@@ -315,24 +356,23 @@ function incomingCallResponse(calleeId, from, callResponse, calleeSdp) {
 	
 	if(callResponse === 'accept') {
 		var pipeline = new CallMediaPipeline();
+		pipelines[caller.id] = pipeline;
+		pipelines[callee.id] = pipeline;
 
-		pipeline.createPipeline(function(error) {
+		pipeline.createPipeline(caller.id, callee.id, ws, function(error) {
 			if(error) {
 				return onError(error, error);
 			}
 			
-			pipeline.generateSdpAnswerForCaller(caller.sdpOffer, function(error, callerSdpAnswer) {
+			pipeline.generateSdpAnswer(caller.id, caller.sdpOffer, function(error, callerSdpAnswer) {
 				if(error) {
 					return onError(error, error);
 				}
 				
-				pipeline.generateSdpAnswerForCallee(calleeSdp, function(error, calleeSdpAnswer) {
+				pipeline.generateSdpAnswer(callee.id, calleeSdp, function(error, calleeSdpAnswer) {
 					if(error) {
 						return onError(error, error);
 					}
-					
-					pipelines[caller.id] = pipeline;
-					pipelines[callee.id] = pipeline;
 					
 					var message = {
 						id: 'startCommunication',
@@ -356,6 +396,88 @@ function incomingCallResponse(calleeId, from, callResponse, calleeSdp) {
 			message: 'user declined'
 		};
 		
+		caller.sendMessage(decline);
+	}
+};
+
+function incomingCallResponse(calleeId, from, callResponse, calleeSdp, ws) {
+    clearCandidatesQueue(calleeId);
+
+	function onError(callerReason, calleeReason) {
+    	if(pipeline) {
+        	pipeline.release();
+        }
+        
+    	if(caller) {
+    		var callerMessage = {
+    			id: 'callResponse',
+    			response: 'rejected'
+    		}
+    		
+    		if(callerReason) {
+    			callerMessage.message = callerReason;
+    		}
+    		caller.sendMessage(callerMessage);
+    	}
+
+    	var calleeMessage = {
+    		id: 'stopCommunication'
+    	};
+    	
+    	if(calleeReason) {
+        	calleeMessage.message = calleeReason;
+    	}
+    	
+    	callee.sendMessage(calleeMessage);
+	}
+
+	var callee = userRegistry.getById(calleeId);
+	if(!from || !userRegistry.getByName(from)) {
+		return onError(null, 'unknown from = ' + from);
+	}
+	var caller = userRegistry.getByName(from);
+
+	if(callResponse === 'accept') {
+		var pipeline = new CallMediaPipeline();
+		pipelines[caller.id] = pipeline;
+		pipelines[callee.id] = pipeline;
+
+		pipeline.createPipeline(caller.id, callee.id, ws, function(error) {
+			if(error) {
+				return onError(error, error);
+			}
+
+			pipeline.generateSdpAnswer(caller.id, caller.sdpOffer, function(error, callerSdpAnswer) {
+				if(error) {
+					return onError(error, error);
+				}
+
+				pipeline.generateSdpAnswer(callee.id, calleeSdp, function(error, calleeSdpAnswer) {
+					if(error) {
+						return onError(error, error);
+					}
+
+					var message = {
+						id: 'startCommunication',
+						sdpAnswer: calleeSdpAnswer
+					};
+					callee.sendMessage(message);
+
+					message = {
+						id: 'callResponse',
+						response : 'accepted',
+						sdpAnswer: callerSdpAnswer
+					};
+					caller.sendMessage(message);
+				});
+			});
+		});
+	} else {
+		var decline = {
+			id: 'callResponse',
+			response: 'rejected',
+			message: 'user declined'
+		};
 		caller.sendMessage(decline);
 	}
 };
@@ -384,6 +506,8 @@ function stop(sessionId) {
 			
 			stoppedUser.sendMessage(message)
 		}
+		
+		clearCandidatesQueue(sessionId);
 	}
 };
 
@@ -405,3 +529,28 @@ function getKurentoClient(callback) {
 		callback(null, kurentoClient);
 	});
 };
+
+function clearCandidatesQueue(sessionId) {
+    if (candidatesQueue[sessionId]) {
+        delete candidatesQueue[sessionId];
+    }
+}
+
+function onIceCandidate(sessionId, _candidate) {
+    var candidate = kurento.register.complexTypes.IceCandidate(_candidate);
+    var user = userRegistry.getById(sessionId);
+
+    if (pipelines[user.id] && pipelines[user.id].webRtcEndpoint && pipelines[user.id].webRtcEndpoint[user.id]) {
+        var webRtcEndpoint = pipelines[user.id].webRtcEndpoint[user.id];
+        webRtcEndpoint.addIceCandidate(candidate);
+    }
+    else {
+        if (!candidatesQueue[user.id]) {
+            candidatesQueue[user.id] = [];
+        }
+        candidatesQueue[sessionId].push(candidate);
+    }
+}
+
+app.use(express.static(path.join(__dirname, 'static')));
+
